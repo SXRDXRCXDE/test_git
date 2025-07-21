2bc8126fd88d75d59479b2ffb741a28d6e0a9a4d
SXRDXR <sardorboynazarv@gmail.com>
2025-07-21 16:11:18 +0500

    Sardor version entered

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Contact(){
+    return(
+        <div className={`w-screen h-screen bg-gray-800 flex items-center justify-center text-white text-5xl font-bold`}>
+            <span>Beat it Down</span>
+        </div>
+    );
+};
