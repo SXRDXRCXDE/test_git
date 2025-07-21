ddd22cb8ecb8255dd755b62f2fbe4dfa9f2ce6b5
SXRDXR <sardorboynazarv@gmail.com>
2025-07-21 16:14:08 +0500

    new changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,6 +4,7 @@ export default function Contact(){
     return(
         <div className={`w-screen h-screen bg-gray-800 flex items-center justify-center text-white text-5xl font-bold`}>
             <span>Beat it Down</span>
+            <span>I can fully control my branch huzz</span>
         </div>
     );
 };
