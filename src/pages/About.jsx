bc1e21823544b286f2e04104d1d566ef80b76cf7
SXRDXR <sardorboynazarv@gmail.com>
2025-07-21 12:28:17 +0500

    Change to test_branch

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function About() {
+
+    return(
+        <div className={'w-screen h-screen bg-green-600 flex items-center justify-center text-5xl font-bold'}>
+            <span>This About Us</span>
+        </div>
+    );
+
+}
