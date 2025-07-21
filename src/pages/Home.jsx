6b59aa72d76615b982366e85ec38443e79768232
SXRDXR <sardorboynazarv@gmail.com>
2025-07-21 11:19:58 +0500

    Second Commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function Home() {
+    return(
+        <div className={'w-screen h-screen bg-amber-600 flex items-center justify-center text-5xl font-bold'}>
+            <span>This is HomePage</span>
+        </div>
+    );
+}
